a9e6f35e7d97291f7c9e2a5cdbbecfc12fa429d4
fatmaguent <guentfatma4@gmail.com>
2025-03-13 02:40:56 +0100

    Fusion squash de feature_list dans main

--- main.py
+++ main.py
@@ -8,5 +8,10 @@ my_list = [1, 5, 7]
 som = somme(my_list)
 print('La somme est =', som)
 my_list = [1, 5, 7]
-print('La somme est', sum(my_list))
+if my_list:
+    print('La somme est :', sum(my_list))
+    print('Le max est :', max(my_list))
+    print('Le min est :', min(my_list))
+else:
+    print('Liste vide')
 
